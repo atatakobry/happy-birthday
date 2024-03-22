edf7f609c6736ea7fe5cfd5074b21259abe82322
atatakorby <atatakorby@gmail.com>
2024-03-22 14:52:08 +0200

    minor fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App.tsx';
+import App from './App';
 
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
